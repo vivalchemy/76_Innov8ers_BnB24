854c992381a564a4cdda3112ffe6dc30e34ceda2
vivalchemy <vivalchemy.social+github@gmail.com>
2024-02-18 00:17:12 +0530

    Got the homepage file from pratyay due to severe merge conflict

---
+++ js/hommepage.js
@@ -0,0 +1,26 @@
+function voter1() {
+  document.getElementById("voter1").innerText = "20";
+}
+function voter2() {
+  document.getElementById("voter2").innerText = "40";
+}
+function voter3() {
+  document.getElementById("voter3").innerText = "40";
+}
+function voter4() {
+  document.getElementById("voter4").innerText = "10";
+}
+function voter5() {
+  document.getElementById("voter5").innerText = "10";
+}
+function voter6() {
+  document.getElementById("voter6").innerText = "20";
+}
+function normal() {
+  document.getElementById("voter1").innerText = "Upvote";
+  document.getElementById("voter2").innerText = "Downvote";
+  document.getElementById("voter3").innerText = "Upvote";
+  document.getElementById("voter4").innerText = "Downvote";
+  document.getElementById("voter5").innerText = "Upvote";
+  document.getElementById("voter6").innerText = "Downvote";
+}
